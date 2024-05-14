85946e495357990051f004c48f993a25bf44473f
Mridul Markandya <mridulmarkandya96@gmail.com>
2024-05-14 21:00:17 +0530

    app.py

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ from collections import Counter
 from PIL import Image
 
 class YOLO:
-    """
-    This class loads and performs inference with a YOLOv8 model.
-    """
     def __init__(self, weights_path):
-        self.model = torch.hub.load('ultralytics/yolov8', 'yolov8n', weights='/best.pt')
+        self.model = torch.hub.load('ultralytics/yolov8', 'yolov8n', weights='best.pt')
 
     def __call__(self, images):
-        # Assuming images is a list of PIL Images
         results = self.model(images)
         return results
 
 def count_objects(results):
   total_objects = 0
   for result in results:
-    total_objects += len(result.boxes)  # Assuming boxes attribute holds detections
+    total_objects += len(result.boxes)
   return total_objects
 
 
@@ -31,15 +27,13 @@ def main():
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    # Load YOLO model
-    model = YOLO('/best.pt')  # Replace with actual path
+    
+    model = YOLO('best.pt')
 
-    images = [image]  # Convert to list for model inference
+    images = [image]
 
-    # Run inference with YOLO
     results = model(images)
 
-    # Count objects
     total_objects = count_objects(results)
 
     st.success(f'Total objects detected: {total_objects}')
